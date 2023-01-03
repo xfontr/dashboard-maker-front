c038822f914546bcfee22a9ecd12e9bf1f1d07a4
Xifre Font <fontx@es-embp2022.es.lidl.net>
2023-01-03 09:04:58 +0100

    Test form utils

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,6 @@
 // import { FormSchema } from "./common/components/Form/Form.types";
 // import { AnimatedButton } from "./common/components/Button/Button";
 
-// const mockFormSchema: FormSchema = [
-//   {
-//     label: "Hello",
-//     fieldProps: {
-//       className: "experiment",
-//     },
-//     inputProps: {
-//       id: "Pedro",
-//       placeholder: "Hello dawg",
-//     },
-//   },
-// ];
-
 const App = (): JSX.Element => (
   <div className="app">
     {/* <Form schema={mockFormSchema}>
---
+++ src/common/test-utils/mocks/mockFormSchema.ts
@@ -0,0 +1,16 @@
+import { FormSchema } from "../../components/Form/Form.types";
+
+const mockFormSchema: FormSchema = [
+  {
+    label: "Hello",
+    fieldProps: {
+      className: "experiment",
+    },
+    inputProps: {
+      id: "Pedro",
+      placeholder: "Hello dawg",
+    },
+  },
+];
+
+export default mockFormSchema;
