5fc0cd74b5490fa28c8ea377e05a7880f199c9db
Xifre Font <fontx@es-embp2022.es.lidl.net>
2023-01-05 09:42:10 +0100

    Component to render errors only for a specific field

---
+++ src/common/components/Form/PathErrors/PathErrors.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from "@testing-library/react";
+import schema from "../../../test-utils/mocks/mockFormSchema";
+import simplifySchema from "../../../test-utils/simplifySchema";
+import { validateForm } from "../Form.utils";
+import PathErrors from "./PathErrors";
+
+describe("Given a PathErrors component", () => {
+  describe("When instantiated with a list of errors and a input id", () => {
+    test("Then it should show all the errors in the list that match the id", () => {
+      const values = simplifySchema(schema);
+      const errors = validateForm(values);
+      const path = schema[0].inputProps.id;
+
+      render(<PathErrors {...{ errors, path }} />);
+
+      errors?.forEach(({ message }) => {
+        const shownError = screen.getByText(message);
+        expect(shownError).toBeInTheDocument();
+      });
+    });
+
+    describe("When instantiated with no props", () => {
+      test("Then it should display nothing", () => {
+        render(<PathErrors path="email" />);
+
+        const notView = screen.queryByTestId("errors");
+
+        expect(notView).not.toBeInTheDocument();
+      });
+    });
+
+    describe("When instantiated with an empty list of errors", () => {
+      test("Then it should display nothing", () => {
+        render(<PathErrors path="email" errors={[]} />);
+
+        const notView = screen.queryByTestId("errors");
+
+        expect(notView).not.toBeInTheDocument();
+      });
+    });
+  });
+});
