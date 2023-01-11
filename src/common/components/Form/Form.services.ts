8a6ca611436ef2a7dd51a810af337c6d38983f37
xfontr <xifre@Xifres-MacBook-Pro.local>
2023-01-11 20:53:17 +0100

    Test service function to generate form schemas

--- src/common/components/Form/Form.services.ts
+++ src/common/components/Form/Form.services.ts
@@ -1,12 +1,18 @@
 import capitalize from "../../utils/capitalize";
 import { FormSchema } from "./Form.types";
 
-//TODO: Test pending
+/**
+ * @example
+ *   SimpleFormFields({ name: "John" })("name", "email");
+ *   // In this case, the field email would be loaded with no initial value
+ *
+ * @param values An containing the id of the field and its initial value
+ */
 
 export const SimpleFormFields =
   (values: Record<string, string | number>) =>
-  (...args: string[]): FormSchema =>
-    args.map((name) => ({
+  (...names: string[]): FormSchema =>
+    names.map((name) => ({
       label: capitalize(name),
       inputProps: {
         id: name,
