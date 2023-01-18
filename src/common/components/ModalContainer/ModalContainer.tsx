63c5547f03b055ca9be3bded1d855e396534a19b
Xifré Font <fontx@es-embp2022.es.lidl.net>
2023-01-18 17:41:24 +0100

    Modal backdrop container of the actual modal

---
+++ src/common/components/ModalContainer/ModalContainer.tsx
@@ -0,0 +1,43 @@
+import { HTMLAttributes, PropsWithChildren, ReactNode } from "react";
+import setProps from "../../utils/setProps";
+import Portal from "../Portal/Portal";
+import "./ModalContainer.scss";
+
+interface ModalContainerProps extends HTMLAttributes<HTMLDialogElement> {
+  children: ReactNode;
+  childrenPosition?: "top" | "center";
+  backDrop?: boolean;
+}
+
+const ModalContainer = ({
+  children,
+  backDrop = false,
+  childrenPosition = "center",
+  ...rest
+}: ModalContainerProps): JSX.Element => {
+  const Dialog = ({ children }: PropsWithChildren) => (
+    <dialog
+      {...setProps(
+        rest,
+        "className",
+        backDrop ? "modal" : `modal modal--${childrenPosition}`
+      )}
+    >
+      {children}
+    </dialog>
+  );
+
+  return (
+    <Portal>
+      {backDrop ? (
+        <div className={`backdrop backdrop--${childrenPosition}`}>
+          {<Dialog>{children}</Dialog>}
+        </div>
+      ) : (
+        <Dialog>{children}</Dialog>
+      )}
+    </Portal>
+  );
+};
+
+export default ModalContainer;
