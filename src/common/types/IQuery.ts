d445d912d05b90f5a8bc686790e353e2f6d71b69
xfontr <xifre@Xifres-MacBook-Pro.local>
2023-01-22 12:05:58 +0100

    Log in form component

---
+++ src/common/types/IQuery.ts
@@ -0,0 +1,15 @@
+import IResponse from "./IResponse";
+
+export type SideEffectsOptions<T> = {
+  successCondition: [keyof T, string | number | boolean];
+  loading?: string;
+  error?: string;
+  success?: string;
+};
+
+export type QueryOptions<T, L> = {
+  onSuccess?: (response: IResponse<T>, values?: L) => unknown;
+  onError?: (response: IResponse<T>) => unknown;
+  onInit?: () => void;
+  options: SideEffectsOptions<IResponse<T>>;
+};
---
+++ src/features/users/components/LogInForm/LogIn.form.tsx
@@ -0,0 +1,21 @@
+import Button from "../../../../common/components/Button/Button";
+import Form from "../../../../common/components/Form/Form";
+import useForm from "../../../../common/hooks/useForm";
+import useLogIn from "../../hooks/useLogIn";
+import { UserLogInData } from "../../types/user.types";
+import logInSchema from "./LogIn.schema";
+
+const LogInForm = (): JSX.Element => {
+  const handleSubmit = useLogIn();
+  const formHandler = useForm<UserLogInData>(logInSchema, handleSubmit);
+
+  return (
+    <Form {...{ formHandler }}>
+      <div className="form__buttons">
+        <Button>Log in</Button>
+      </div>
+    </Form>
+  );
+};
+
+export default LogInForm;
