8d01d6397124990c61177246fca37cf8cdd78301
Xifré Font <fontx@es-embp2022.es.lidl.net>
2023-01-24 19:27:53 +0100

    Refresh token hook basic logic

---
+++ src/features/users/hooks/useRefreshToken.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { api } from "../../../common/services/RequestHandler";
+import { MAIN_IDENTIFIER } from "../../../config/database";
+import ENDPOINTS from "../../../config/endpoints";
+import { logInActionCreator, setRefreshTokenActionCreator } from "../store";
+import useUserAuth from "../store/userAuth.hook";
+import { CodedToken } from "../types/token.types";
+import decodeToken from "../utils/decodeToken";
+
+const useRefreshToken = () => {
+  const { dispatch, userAuth } = useUserAuth();
+  const [isLogging, setIsLogging] = useState<boolean>(true);
+
+  useEffect(() => {
+    (async () => {
+      if ((!isLogging && !userAuth.isLogged) || userAuth.isLogged) return;
+
+      const { body: refreshToken } = await api.get<CodedToken>(
+        ENDPOINTS.users.refreshToken,
+        { withCredentials: true }
+      );
+
+      if (isLogging) {
+        const { email, role } = decodeToken(refreshToken?.user.token!);
+
+        dispatch(
+          logInActionCreator({
+            email,
+            authToken: refreshToken?.user.token!,
+            role,
+          })
+        );
+
+        setIsLogging(false);
+        return;
+      }
+
+      dispatch(setRefreshTokenActionCreator(refreshToken?.user.token));
+    })();
+  }, [dispatch, isLogging, userAuth.isLogged]);
+};
+
+export default useRefreshToken;
