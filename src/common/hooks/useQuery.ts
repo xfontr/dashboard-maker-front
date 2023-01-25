313649f393352172ec5e108750dcff8e64bee2af
Xifré Font <fontx@es-embp2022.es.lidl.net>
2023-01-25 16:39:37 +0100

    Test useQuery hook

--- src/common/hooks/useQuery.ts
+++ src/common/hooks/useQuery.ts
@@ -3,9 +3,35 @@ import { QueryOptions } from "../types/IQuery";
 import IResponse from "../types/IResponse";
 import callFunctionIfExists from "../utils/callFunctionIfExists";
 
-// TODO: Document all of this very carefully once completely finished
-
-// TODO: Test all the side effects ecosystem
+/**
+ * Takes a custom object of options that defines the UI behaviour and forces an
+ * error if the response doesn't match a specific status
+ *
+ * It returns a function that receives a callback, which is the one that will be
+ * doing the request. This function also returns another one, which is the one
+ * that contains the main logic and receives values in case they are needed
+ *
+ * The options can include init, success and error actions to be performed
+ *
+ * @example
+ *   const logIn = useQuery<CodedToken, UserLogInData>({
+ *   onSuccess: (response, passedValues) => {},
+ *   onError: (response) => {},
+ *   onInit: () => {},
+ *   options: {
+ *   successCondition: ["status", 200],
+ *   loading: "Loading...",
+ *   error: "Something went wrong",
+ *   // success: "" --> If not specified, it will not launch the UI that matches the status
+ *   },
+ *   });
+ *
+ *   // The callback in the curried function receives the input of data
+ *   const logInPetition = logIn((passedValues) => api.post(passedValues))
+ *
+ *   // And this is the final actual request:
+ *   logInPetition({ email: test@test.com })
+ */
 
 const useQuery = <T = unknown, L = unknown>(options: QueryOptions<T, L>) => {
   const { showLoadingUi, showErrorUi, showSuccessUi } = useUiMiddlewares<
@@ -17,7 +43,28 @@ const useQuery = <T = unknown, L = unknown>(options: QueryOptions<T, L>) => {
     callFunctionIfExists(options.onInit);
   };
 
+  /**
+   * @example
+   *   const example = useQuery(...args);
+   *
+   *   const finalCall = example((passedValues) => api.post(passedValues));
+   *
+   *   finalCall(passedValues); // Here's where the passedValues come from
+   *
+   * @param callback (passedValues: object) => Promise<response>
+   *
+   *   Function returned by the useQuery hook that takes a callback function as a
+   *   parameter, which will be called by said hook and receive the values
+   *   passed by the user
+   */
+
   return (callback: (values?: L) => Promise<IResponse<T>>) =>
+    /**
+     * Last function returned by the useQuery hook. Takes optional values that
+     * will be added to the request of the previous callback. Once called, it
+     * calls the callback and then does the success/error actions depending on
+     * the outcome
+     */
     async (values?: L): Promise<unknown | IResponse<T>> => {
       init();
 
