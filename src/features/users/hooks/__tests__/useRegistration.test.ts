828717be9196b1ee01a52a5e2d4b3716f7694b23
Xifré Font <fontx@es-embp2022.es.lidl.net>
2023-01-25 17:22:59 +0100

    Added corner cases to the use registration hook test

--- src/features/users/hooks/__tests__/useRegistration.test.ts
+++ src/features/users/hooks/__tests__/useRegistration.test.ts
@@ -1,5 +1,4 @@
 /* eslint-disable testing-library/no-unnecessary-act */
-import { renderHook } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import useSteps from "../../../../common/hooks/useSteps";
 import { mockProtoToken } from "../../../../common/test-utils/mocks/mockToken";
@@ -8,6 +7,12 @@ import { FORCE_ERROR } from "../../../../common/test-utils/mockServer/mockHandle
 import { MAIN_IDENTIFIER } from "../../../../config/database";
 import ENDPOINTS from "../../../../config/endpoints";
 import useRegistration from "../useRegistration";
+import { ProtoToken } from "../../types/token.types";
+import { renderHook } from "../../../../common/test-utils/customRender";
+import { useUi } from "../../../../common/store/slices/ui";
+import { SIGN_USER_UP_UI, VERIFY_TOKEN_UI } from "../../config/ui.constants";
+import { waitFor } from "@testing-library/react";
+import { wait } from "@testing-library/user-event/dist/types/utils";
 
 const originalEndpoints = {
   tokens: { ...ENDPOINTS.tokens },
@@ -25,67 +30,73 @@ describe("Given a useRegistration hook", () => {
     const next = jest.fn() as ReturnType<typeof useSteps>["next"];
 
     describe("And when called its returned function handleTokenSubmit with token values", () => {
-      test("Then it should update the user token and call next", async () => {
-        const { result, rerender } = renderHook(useRegistration, {
-          initialProps: next,
-        });
+      test("Then it should update the user token, the ui, and call next", async () => {
+        const { result } = renderHook(() => ({
+          useRegistration: useRegistration(next),
+          ui: useUi(),
+        }));
 
-        expect(result.current.user).toBeUndefined();
-        expect(result.current.token).toBeUndefined();
+        expect(result.current.useRegistration.user).toBeUndefined();
+        expect(result.current.useRegistration.token).toBeUndefined();
+
+        expect(result.current.ui.ui.status).toBe("IDLE");
 
         await act(async () => {
-          await result.current.handleTokenSubmit(
-            mockProtoToken as Record<string, string>
+          await result.current.useRegistration.handleTokenSubmit(
+            mockProtoToken as ProtoToken
           );
-
-          rerender();
         });
 
-        expect(result.current.token!.code).toBe(mockProtoToken.code);
-        expect(result.current.user![MAIN_IDENTIFIER]).toBe(
+        expect(result.current.useRegistration.token!.code).toBe(
+          mockProtoToken.code
+        );
+        expect(result.current.useRegistration.user![MAIN_IDENTIFIER]).toBe(
           mockProtoToken[MAIN_IDENTIFIER]
         );
 
         expect(next).toHaveBeenCalled();
+
+        // UI
+        expect(result.current.ui.ui.status).toBe("SUCCESS");
+        expect(result.current.ui.ui.message).toBe(VERIFY_TOKEN_UI.success);
       });
 
       test("Then it should do nothing if there's an error while requesting the token", async () => {
         ENDPOINTS.tokens.verify = FORCE_ERROR;
 
-        const { result, rerender } = renderHook(useRegistration, {
-          initialProps: next,
-        });
+        const { result } = renderHook(() => ({
+          useRegistration: useRegistration(next),
+          ui: useUi(),
+        }));
+
+        expect(result.current.ui.ui.status).toBe("IDLE");
 
         await act(async () => {
-          await result.current.handleTokenSubmit(
-            mockProtoToken as Record<string, string>
+          await result.current.useRegistration.handleTokenSubmit(
+            mockProtoToken as ProtoToken
           );
-
-          rerender();
         });
 
-        expect(result.current.user).toBeUndefined();
-        expect(result.current.token).toBeUndefined();
+        expect(result.current.useRegistration.user).toBeUndefined();
+        expect(result.current.useRegistration.token).toBeUndefined();
 
         expect(next).not.toHaveBeenCalled();
+
+        // UI
+        expect(result.current.ui.ui.status).toBe("ERROR");
+        expect(result.current.ui.ui.message).toBe(VERIFY_TOKEN_UI.error);
       });
     });
 
     describe("And when called its returned function handlePasswordSubmit with user values", () => {
       test("Then it should update the user and call next", () => {
-        const { result, rerender } = renderHook(useRegistration, {
-          initialProps: next,
-        });
+        const { result } = renderHook(() => useRegistration(next));
 
         expect(result.current.user).toBeUndefined();
         expect(result.current.token).toBeUndefined();
 
         act(() => {
-          result.current.handlePasswordSubmit(
-            mockProtoUser as unknown as Record<string, string>
-          );
-
-          rerender();
+          result.current.handlePasswordSubmit(mockProtoUser);
         });
 
         expect(result.current.user).toStrictEqual(mockProtoUser);
@@ -94,58 +105,68 @@ describe("Given a useRegistration hook", () => {
     });
 
     describe("And when called its returned function handleSignUpSubmit with user values", () => {
-      test("Then it should update the user token and call next", async () => {
-        const { result, rerender } = renderHook(useRegistration, {
-          initialProps: next,
-        });
+      test("Then it should register the user with the passed values, update the ui and call next", async () => {
+        const { result } = renderHook(() => ({
+          useRegistration: useRegistration(next),
+          ui: useUi(),
+        }));
 
-        expect(result.current.user).toBeUndefined();
-        expect(result.current.token).toBeUndefined();
+        expect(result.current.useRegistration.user).toBeUndefined();
+        expect(result.current.useRegistration.token).toBeUndefined();
+
+        expect(result.current.ui.ui.status).toBe("IDLE");
 
         await act(async () => {
-          await result.current.handleTokenSubmit(
-            mockProtoToken as Record<string, string>
+          await result.current.useRegistration.handleTokenSubmit(
+            mockProtoToken as ProtoToken
           );
         });
 
         await act(async () => {
-          await result.current.handleSignUpSubmit(
-            mockProtoUser as unknown as Record<string, string>
+          await result.current.useRegistration.handleSignUpSubmit(
+            mockProtoUser
           );
-
-          rerender();
         });
 
-        expect(result.current.user).toStrictEqual(mockProtoUser);
+        expect(result.current.useRegistration.user).toStrictEqual(
+          mockProtoUser
+        );
         expect(next).toHaveBeenCalledTimes(2);
+
+        // UI
+        expect(result.current.ui.ui.status).toBe("SUCCESS");
+        expect(result.current.ui.ui.message).toBe(SIGN_USER_UP_UI.success);
       });
 
-      test("Then it should not call next if there's an error while requesting the token", async () => {
+      test("Then it should not call next if there's an error while registering the user, and update the ui", async () => {
         ENDPOINTS.users.signUp = FORCE_ERROR;
 
-        const { result, rerender } = renderHook(useRegistration, {
-          initialProps: next,
-        });
+        const { result } = renderHook(() => ({
+          useRegistration: useRegistration(next),
+          ui: useUi(),
+        }));
 
-        expect(result.current.user).toBeUndefined();
-        expect(result.current.token).toBeUndefined();
+        expect(result.current.useRegistration.user).toBeUndefined();
+        expect(result.current.useRegistration.token).toBeUndefined();
+
+        expect(result.current.ui.ui.status).toBe("IDLE");
 
         await act(async () => {
-          await result.current.handleTokenSubmit(
-            mockProtoToken as Record<string, string>
+          await result.current.useRegistration.handleTokenSubmit(
+            mockProtoToken as ProtoToken
           );
         });
 
         await act(async () => {
-          await result.current.handleSignUpSubmit(
-            mockUser as unknown as Record<string, string>
-          );
-
-          rerender();
+          await result.current.useRegistration.handleSignUpSubmit(mockUser);
         });
 
-        expect(result.current.user).toStrictEqual(mockUser);
+        expect(result.current.useRegistration.user).toStrictEqual(mockUser);
         expect(next).toHaveBeenCalledTimes(1);
+
+        // UI
+        expect(result.current.ui.ui.status).toBe("ERROR");
+        expect(result.current.ui.ui.message).toBe(SIGN_USER_UP_UI.error);
       });
     });
   });
