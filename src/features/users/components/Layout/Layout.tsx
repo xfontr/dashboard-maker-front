002ce643b67f5c9730c5e2afab071ad8e2003e72
xfontr <xifre@Xifres-MacBook-Pro.local>
2023-02-05 22:24:10 +0100

    Layout mock logo and background image

--- src/features/users/components/Layout/Layout.tsx
+++ src/features/users/components/Layout/Layout.tsx
@@ -1,15 +1,20 @@
 import { PropsWithChildren } from "react";
+import { GlassBox } from "../../../../common/components/Box/Box";
 import "./Layout.scss";
 
 const Layout = ({ children }: PropsWithChildren) => (
-  <>
-    <main className="main">
-      <div className="company">
-        <img src="" alt="Company logo" />
-      </div>
-      <div className="content">{children}</div>
-    </main>
-  </>
+  <main className="main">
+    <header className="main__company">
+      <img
+        src="./img/mock-logo.png"
+        alt="Company logo"
+        height={200}
+        width={200}
+      />
+    </header>
+
+    <GlassBox className="main__content">{children}</GlassBox>
+  </main>
 );
 
 export default Layout;
