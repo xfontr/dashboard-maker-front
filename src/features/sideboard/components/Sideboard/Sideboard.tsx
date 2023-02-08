bf687ae1edfd6b1f1cd72f701704e8add5e36807
xfontr <xifre@Xifres-MacBook-Pro.local>
2023-02-08 22:31:59 +0100

    Expandable sideboard basic logic and item basic implementation

--- src/features/sideboard/components/Sideboard/Sideboard.tsx
+++ src/features/sideboard/components/Sideboard/Sideboard.tsx
@@ -1,16 +1,40 @@
 import useLogOut from "../../../users/hooks/useLogOut";
-import Button from "../../../../common/components/Button/Button";
 import "./Sideboard.scss";
+import MenuItem from "../MenuItem/MenuItem";
+import { CloseIcon } from "../../../../common/components/Icon/Icon";
+import { useNavigate } from "react-router-dom";
+import PATHS from "../../../../config/paths";
+import { useState } from "react";
 
 const Sideboard = (): JSX.Element => {
+  const [showOnlyIcon, setIsExpanded] = useState<boolean>(true);
+
   const logOut = useLogOut();
+  const navigate = useNavigate();
 
   return (
-    <aside className="sideboard">
-      <ul>
-        <li>
-          <Button onClick={logOut}>Log out</Button>
-        </li>
+    <aside
+      className={`sideboard${showOnlyIcon ? " sideboard--contracted" : ""}`}
+    >
+      <ul className="sideboard__items">
+        <MenuItem
+          label="Home"
+          onClick={() => navigate(PATHS.home)}
+          Icon={CloseIcon}
+          {...{ showOnlyIcon }}
+        />
+        <MenuItem
+          label="Log out"
+          onClick={logOut}
+          Icon={CloseIcon}
+          {...{ showOnlyIcon }}
+        />
+        <MenuItem
+          label="Contract/Expand"
+          onClick={() => setIsExpanded((current) => !current)}
+          Icon={CloseIcon}
+          {...{ showOnlyIcon }}
+        />
       </ul>
     </aside>
   );
