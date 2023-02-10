29f90788bbee64b607cf8da893b44bf159b3e509
xfontr <xifre@Xifres-MacBook-Pro.local>
2023-02-10 17:04:16 +0100

    Apply basic styles and adjust test

--- src/common/components/Breadcrumbs/BreadCrumbs.tsx
+++ src/common/components/Breadcrumbs/BreadCrumbs.tsx
@@ -1,13 +1,18 @@
+import { HTMLAttributes } from "react";
 import { Link, useLocation } from "react-router-dom";
 import PATHS from "../../../config/paths";
+import setProps from "../../utils/setProps";
+import "./Breadcrumbs.scss";
 
-const Breadcrumbs = (): JSX.Element => {
+interface BreadcrumbsProps extends HTMLAttributes<HTMLDivElement> {}
+
+const Breadcrumbs = ({ ...rest }: BreadcrumbsProps): JSX.Element => {
   const { pathname } = useLocation();
   const userPaths = pathname.split("/");
   userPaths.splice(0, 1);
 
   return (
-    <div className="breadcrumbs">
+    <div {...setProps(rest, "className", "breadcrumbs")}>
       <Link key={"home"} to={PATHS.root}>
         home
       </Link>
--- src/common/components/Breadcrumbs/Breadcrumbs.test.tsx
+++ src/common/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -3,12 +3,15 @@ import { baseWrapper } from "../../test-utils/Wrapper";
 import Breadcrumbs from "./BreadCrumbs";
 
 describe("Given a Breadcrumbs component", () => {
-  describe("When instantiated and the route is '/news/fake-page'", () => {
+  describe("When instantiated with a class 'Test' and the route is '/news/fake-page'", () => {
     test("Then it should display a link to home and /fake-page, and a text '/news'", () => {
+      const className = "Test";
       const falsePath = "/news/fake-page";
       const Wrapper = baseWrapper([falsePath]);
 
-      render(<Breadcrumbs />, { wrapper: Wrapper });
+      render(<Breadcrumbs data-testid="breadcrumbs" {...{ className }} />, {
+        wrapper: Wrapper,
+      });
 
       const breadcrumbs = [
         screen.getByRole("link", { name: "home" }),
@@ -17,6 +20,9 @@ describe("Given a Breadcrumbs component", () => {
       ];
 
       breadcrumbs.forEach((path) => expect(path).toBeInTheDocument());
+
+      const wrapper = screen.getByTestId("breadcrumbs");
+      expect(wrapper).toHaveClass(className);
     });
   });
 });
